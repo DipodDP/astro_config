e90e8610998e8723ae593755dc472dafb72fc925
DipodDP <dipoddp@gmail.com>
2023-06-02 11:52:27 +0000

    feat(config): included Kabinspace config

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,7 +5,7 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   -- { -- further customize the options set by the community
   --   "copilot.lua",
   --   opts = {
--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,39 +1,27 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
--- <<<<<<< HEAD
---   opts = function(_, opts)
---     local null_ls = require "null-ls"
---     opts.sources = {
---
---       -- null_ls.builtins.formatting.stylua,
---       null_ls.builtins.formatting.prettier,
---       null_ls.builtins.formatting.black,
---       null_ls.builtins.formatting.isort,
---       -- null_ls.builtins.formatting.clang_format,
---       -- null_ls.builtins.formatting.rustfmt,
---       null_ls.builtins.formatting.shfmt.with {
---         args = { "-i", "2" },
---       },
---       -- null_ls.builtins.diagnostics.luacheck,
---       null_ls.builtins.diagnostics.flake8,
---       -- null_ls.builtins.diagnostics.pylint,
---       -- null_ls.builtins.diagnostics.mypy,
---     }
---     return opts
--- =======
-  opts = function(_, config)
-    -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
-
-    -- Check supported formatters and linters
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    config.sources = {
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+-- config variable is the default configuration table for the setup function call
+-- Check supported formatters and linters
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+  opts = function(_, opts)
+    local null_ls = require "null-ls"
+    opts.sources = {
+     -- Set a formatter
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.black,
+      null_ls.builtins.formatting.isort,
+      -- null_ls.builtins.formatting.clang_format,
+      -- null_ls.builtins.formatting.rustfmt,
+      null_ls.builtins.formatting.shfmt.with {
+        args = { "-i", "2" },
+      },
+      -- null_ls.builtins.diagnostics.luacheck,
+      null_ls.builtins.diagnostics.flake8,
+      -- null_ls.builtins.diagnostics.pylint,
+      -- null_ls.builtins.diagnostics.mypy,
     }
-    return config -- return final config table
--- >>>>>>> 3492639 (Initial commit)
+    return opts -- return final config table
   end,
 }
